24528c089410a09c09bbf64d05a9eaa263da8213
hamda285 <hamdi.asseir1910@gmail.com>
2024-11-30 02:24:56 +0300

    adding toggle script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+//toggle
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
 
+// Toggle the navigation menu when hamburger is clicked
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
