83756e69793ba8edb0f1944af19a234983e3b1b0
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-02 01:21:05 +0300

    added global variables business list js file

--- js/allBusinesses.js
+++ js/allBusinesses.js
@@ -3,3 +3,8 @@ const cardsPerPage = 8;
 let currentPage = 1;
 let businessData = [];
 let filteredData = [];
+
+const cardContainer = document.getElementById('card-container');
+const paginationContainer = document.getElementById('pagination');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-btn');
