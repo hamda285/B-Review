13ac39373bd0e9a92721e5fc2702a6df1669e4d1
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-01 13:45:04 +0300

    addded retrieve users from local storage  login file of js

--- js/logIn.js
+++ js/logIn.js
@@ -6,3 +6,6 @@ const passwordInput = document.querySelector('#password');
 //Add login form
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault(); // Prevent default form submission
+
+        // Retrieve users from local storage
+        const users = JSON.parse(localStorage.getItem('users')) || [];
