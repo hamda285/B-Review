b7cbea292879035005dd44f21d382e3c9eae72b5
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-02 01:33:19 +0300

    added loaded display user on header business list js file

--- js/allBusinesses.js
+++ js/allBusinesses.js
@@ -125,3 +125,9 @@ const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 const usernameDisplay = document.getElementById('username-display');
 const logoutButton = document.getElementById('logout-button');
 
+// Display username if logged in
+if (currentUser) {
+    usernameDisplay.textContent = `${currentUser.username}`;
+} else {
+    usernameDisplay.textContent = ''; // Hide if not logged in
+}
