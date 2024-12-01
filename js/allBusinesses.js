ac829e07dd2df1f4086f58a8df2b925e10029c12
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-02 01:22:08 +0300

    added cheack localstorage business list js file

--- js/allBusinesses.js
+++ js/allBusinesses.js
@@ -8,3 +8,12 @@ const cardContainer = document.getElementById('card-container');
 const paginationContainer = document.getElementById('pagination');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-btn');
+
+//business lists
+if (window.location.pathname.includes("allBusinesses.html")) {
+    // Check if business data is in localStorage
+    if (localStorage.getItem('businessData')) {
+        businessData = JSON.parse(localStorage.getItem('businessData'));
+        filteredData = businessData;
+        displayCards(currentPage);
+        updatePagination();
