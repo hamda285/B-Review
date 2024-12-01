7b745eb0243067f5228fd7155d456bab7442d41a
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-01 13:44:14 +0300

    addded event listener login file of js

--- js/logIn.js
+++ js/logIn.js
@@ -2,3 +2,7 @@
 const loginForm = document.querySelector('#loginForm');
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
+
+//Add login form
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevent default form submission
