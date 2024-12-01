04b5460490a6b83e2e7fd40e45d504d2dd105331
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-01 13:42:43 +0300

    updated login file of js

--- js/logIn.js
+++ js/logIn.js
@@ -1,3 +1,4 @@
 //=====================login==========================
 const loginForm = document.querySelector('#loginForm');
 const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
