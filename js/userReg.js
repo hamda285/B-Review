ffceb883e2876ac72779c551ff3ac61f059947be
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-01 12:12:38 +0300

    added user registration file in js

---
+++ js/userReg.js
@@ -0,0 +1,8 @@
+//========user registration==============
+const userRegistrationForm = document.getElementById('userRegistrationForm');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const error = document.getElementById('error');
+const success = document.getElementById('suc');
