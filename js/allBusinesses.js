46a7f6959c768c356bbe87899bef6d4c2d7ab87b
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-02 01:32:35 +0300

    added loaded current user business list js file

--- js/allBusinesses.js
+++ js/allBusinesses.js
@@ -120,3 +120,8 @@ if (window.location.pathname.includes("allBusinesses.html")) {
     };
 }
 
+//Load current user information from local storage
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+const usernameDisplay = document.getElementById('username-display');
+const logoutButton = document.getElementById('logout-button');
+
