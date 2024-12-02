bf717bb17244533cb3b0e68d7b27194e7fbc7e1b
hamda285 <hamdi.asseir1910@gmail.com>
2024-12-02 18:16:43 +0300

    created review js file

---
+++ js/review.js
@@ -0,0 +1,13 @@
+
+//====================header.js==================
+// Load current user information from local storage
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+const usernameDisplay = document.getElementById('username-display');
+const logoutButton = document.getElementById('logout-button');
+
+// Display username if logged in
+if (currentUser) {
+    usernameDisplay.textContent = `${currentUser.username}`;
+} else {
+    usernameDisplay.textContent = ''; // Hide if not logged in
+}
